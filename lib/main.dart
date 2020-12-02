16aabc47812ebb1a9915fcf147db22bfe2a7e8b2
sahil1sk <khannasahil303@gmail.com>
2020-12-02 07:26:44 +0530

    Adding an App Theme

--- lib/main.dart
+++ lib/main.dart
@@ -17,7 +17,17 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'FlutterChat',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.pink,
+        backgroundColor: Colors.pink,
+        accentColor: Colors.deepPurple,
+        accentColorBrightness: Brightness.dark, // the text on it should be bright
+        buttonTheme: ButtonTheme.of(context).copyWith(
+          buttonColor: Colors.pink,
+          textTheme: ButtonTextTheme.primary,
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(20),
+          )
+        ),
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
       home: AuthScreen(),//ChatScreen(),
--- lib/widgets/auth/auth_form.dart
+++ lib/widgets/auth/auth_form.dart
@@ -40,6 +40,7 @@ class _AuthFormState extends State<AuthForm> {
                   ),
                   FlatButton(
                     onPressed: () {}, 
+                    textColor: Theme.of(context).primaryColor,
                     child: Text('Create new account'),
                   )
                 ],
