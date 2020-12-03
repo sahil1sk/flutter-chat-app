6939b89cdd6c907c0dcd823030634e2482138fe0
sahil1sk <khannasahil303@gmail.com>
2020-12-03 08:01:45 +0530

    Listening to chat messages

--- lib/screens/chat_screen.dart
+++ lib/screens/chat_screen.dart
@@ -2,6 +2,8 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
+import '../widgets/chat/messages.dart';
+
 class ChatScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
@@ -36,26 +38,15 @@ class ChatScreen extends StatelessWidget {
           ),
         ],
       ),
-      body: StreamBuilder( // StreamBuilder is flutter feature which helps to stream the real time data
-        stream: FirebaseFirestore.instance // so in stream we provide real time data if there is any change in data then builder method will revalute again
-              .collection('/chats/UgBA2FSnEAts1W2pkccO/messages')
-              .snapshots(), 
-        builder: (ctx, streamSnapshot) { // streamSnapshot is the object get access to recieve data
-          if(streamSnapshot.connectionState == ConnectionState.waiting) {
-            return Center(
-              child: CircularProgressIndicator(),
-            );
-          }
-          final documents = streamSnapshot.data.documents; 
-          return ListView.builder(
-            itemCount: documents.length,
-            itemBuilder: (ctx, index) => Container(
-              padding: EdgeInsets.all(8),
-              child: Text(documents[index]['text']),
+      body: Container(
+        child: Column(
+          children: <Widget>[
+            Expanded(
+              child: Messages(),
             ),
-          );
-        },
-      ), 
+          ],
+        ),
+      ),
       floatingActionButton: FloatingActionButton(
         child: Icon(Icons.add),
         onPressed: () {
